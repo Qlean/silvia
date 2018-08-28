ec129e76992a18a573d79fd96e048a1b4e93a255
Дмитрий Мищенко <chromko@adsl-172-10-3-72.dsl.sndg02.sbcglobal.net>
2018-08-28 19:40:21 +0300

    move main to cmd

---
+++ cmd/silvia.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Qlean/silvia/silvia"
+)
+
+func main() {
+	worker := &silvia.Worker{}
+	err := worker.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.Handle("/v1/status", worker.ApiHandler(silvia.StatusApi))
+	http.Handle("/v1/ring", worker.ApiHandler(silvia.RingApi))
+
+	go worker.Generator()
+	go worker.Transformer()
+	go worker.Writer()
+	go worker.Killer()
+
+	log.Println("Server running on port:", worker.Config.Port)
+	http.ListenAndServe(":"+worker.Config.Port, nil)
+}
