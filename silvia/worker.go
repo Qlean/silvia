33293cc029106c9428796a6377e18339e64cde3c
chromko <d.mishchenko@qlean.ru>
2019-03-13 11:37:06 +0300

    increase adjust batch

--- silvia/worker.go
+++ silvia/worker.go
@@ -354,7 +354,7 @@ func (worker *Worker) Writer(driver string) {
 						var query bytes.Buffer
 						query.WriteString(adjustInsert)
 
-						remains := 5
+						remains := 50
 						i := 0
 						for event := range worker.RedshiftAdjustEventBus {
 							i++
